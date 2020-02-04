4641c345ef2e1560c99b1a5fe39276d0153c9d97
Arpit Bansal <arpit.bansal2010@gmail.com>
2020-02-04 22:45:58 +0530

    init contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
+
+const ContactPage = ({}) => (
+  <Layout viewClassName="contact-page">
+    <SEO title="Contact" />
+
+    <Header />
+
+    <div className="wrapper-1200">
+      Email: arpit.bansal2010@gmail.com
+    </div>
+
+    {/* <Footer /> */}
+  </Layout>
+)
+
+export default ContactPage
